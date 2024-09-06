2db92f6d61d33c635d35302724cbba1a2be27321
shafaat Hussain <shafaath508@gmail.com>
2024-09-06 17:47:17 +0530

    This is this

--- helper_functions.js
+++ helper_functions.js
@@ -90,6 +90,7 @@ async function sendOTP(email, username, otp) {
     return responce;
   }
   catch (err) {
+    console.log("Error occured here ");
     console.log("error", err);
     // throw new Error("Error:", err);
   }
